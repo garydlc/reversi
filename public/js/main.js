62bdf2b4f5d1af17b41e72cd2a9f6ebaab910ed9
garydlc <38543439+garydlc@users.noreply.github.com>
2019-02-10 21:59:23 -0800

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -480,7 +480,7 @@ socket.on('game_update', function(payload){
     }
 
     $('#my_color').html('<h3 id="my_color"> I am ' + my_color + '</h3>');
-    $('#my_color').append('<h4>It is '+payload.game.whose_turn+'\s turn. Elapsed time <span id="elapsed"></span></h4>');
+    $('#my_color').append('<h4 class="robotoFont" >It is '+payload.game.whose_turn+'\s turn. Elapsed time <span id="elapsed"></span></h4>');
     clearInterval(interval_timer);
 
     interval_timer = setInterval(function(last_time){
